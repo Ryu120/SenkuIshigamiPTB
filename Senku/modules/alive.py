8f4dd44cc1e5f8bf0449281afa6952a2b93762fd
SOME-1HING <yashprakash2005@gmail.com>
2022-03-31 13:14:33 +0530

    Update alive.py

--- Senku/modules/alive.py
+++ Senku/modules/alive.py
@@ -15,7 +15,8 @@ PHOTO = "https://telegra.ph/file/2b04f7812f22b983f8a10.mp4"
 async def awake(event):
   TEXT = f"**Hi [{event.sender.first_name}](tg://user?id={event.sender.id}), I'm Shikomori Robot.** \n\n"
   TEXT += "⚪ **I'm Working Properly** \n\n"
-  TEXT += f"⚪ **My Head Slave and Creator :Yash @SOME1HING** \n\n"
+  TEXT += f"⚪ **My Head Slave and Creator :Yash SOME1HING**\n\n"
+  TEXT += f"⚪ **My Manager :Sneha @Sneha_UwU_OwO** \n\n"
   TEXT += "**Thanks For Adding Me Here ❤️**"
   BUTTON = [[Button.url("Updates", "https://t.me/Shikimori_bot_Updates"), Button.url("Support", "https://t.me/Shikimori_bot_Support")]]
   await tbot.send_file(event.chat_id, PHOTO, caption=TEXT,  buttons=BUTTON)
